0c1e27348aa17cc350afcfe7bb8f0d7a7a78528d
rohit5126 <rohit5126kumar@gmail.com>
2026-03-12 07:42:52 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,10 +13,10 @@ resource "aws_security_group" "security_group" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 	ingress {
-                from_port = 80
-                to_port = 80
-                protocol = "tcp"
-                cidr_blocks = ["0.0.0.0/0"]
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
         }
 	egress {
 		from_port = 0
